5f722a286438f834052e76d6b37675c14cdea176
unknown <juan.sanchez4.mt@usa.edu.co>
2022-10-03 13:01:48 -0500

    primer commit

---
+++ src/main/java/co/usa/reto3/Reto3/Reto3Application.java
@@ -0,0 +1,12 @@
+package co.usa.reto3.Reto3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3Application {
+
+	public static void main(String[] args) { SpringApplication.run(Reto3Application.class, args);
+	}
+
+}
