6bb393c61553015b67213335d20a5d22623d5570
unknown <juan.sanchez4.mt@usa.edu.co>
2022-10-03 20:33:23 -0500

    Nuevos cambios

---
+++ src/main/java/co/usa/reto3/Reto3/repository/ClientCrudRepository.java
@@ -0,0 +1,7 @@
+package co.usa.reto3.Reto3.repository.crudRepository;
+
+import jdk.jfr.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientCrudRepository extends CrudRepository<Category, Integer> {
+}
